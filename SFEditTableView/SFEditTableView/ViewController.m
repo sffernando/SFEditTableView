707ea3b21281906eed712e84004992d8dfccd3cd
SucreZhang <whusany@163.com>
2015-10-18 23:42:18 +0800

    can reoder

--- SFEditTableView/SFEditTableView/ViewController.m
+++ SFEditTableView/SFEditTableView/ViewController.m
@@ -13,7 +13,8 @@
 
 @property (nonatomic, strong) UITableView *sfTableView;
 @property (nonatomic, strong) NSMutableArray *sfDataArray;
-
+@property (nonatomic, strong) UIButton       *editBtn;
+@property (nonatomic, assign) BOOL           isEditMode;
 
 @end
 
@@ -31,6 +32,14 @@
         _sfDataArray = [SFDataTool sf_tableViewDataSourceWithCount:20];
     }
     
+    _editBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    _editBtn.frame = CGRectMake(self.view.frame.size.width - 80, 17, 80, 30);
+    [_editBtn setTitle:@"Edit" forState:UIControlStateNormal];
+    [_editBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [_editBtn addTarget:self action:@selector(clickToEdit) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_editBtn];
+    
+    
     [_sfTableView reloadData];
 }
 
@@ -39,20 +48,12 @@
     // Dispose of any resources that can be recreated.
 }
 
-//- (UITableView *)sfTableView{
-//    if (_sfTableView == nil) {
-//        _sfTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height - 64) style:UITableViewStylePlain];
-//    }
-//    return _sfTableView;
-//}
-//
-//- (NSMutableArray *)sfDataArray{
-//    if (_sfDataArray == nil) {
-//        _sfDataArray = [SFDataTool sf_tableViewDataSourceWithCount:20];
-//    }
-//    return _sfDataArray;
-//}
+- (void)clickToEdit{
+    _isEditMode = !_isEditMode;
+    [_sfTableView setEditing:_isEditMode animated:YES];
+}
 
+#pragma mark - UITableViewDataSource
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return _sfDataArray.count;
 }
@@ -60,6 +61,7 @@
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     return 60.0f;
 }
+
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     static NSString *cellIdentifier = @"SFTableCell";
@@ -82,6 +84,37 @@
     cell.detailTextLabel.text = personBean.sf_PersonDesc;
     cell.textLabel.text = personBean.sf_PersonName;
     return cell;
+}
+
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
+#pragma mark - Edit Mode
+- (UITableViewCellEditingStyle) tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return UITableViewCellEditingStyleNone; // No Delete icon
+    
+    
+    
+}
+
+
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Can move cell
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
+    
+    NSUInteger origins = sourceIndexPath.row;  // Original position
+    NSUInteger to = destinationIndexPath.row; // Destination position
+    NSLog(@"Origin %lu, To %lu", (unsigned long)origins, (unsigned long)to);
+    
+    [_sfDataArray exchangeObjectAtIndex:origins withObjectAtIndex:to];
 }
 
 @end
